29af381571d426c10386d8e8cad06fdd84e4ec01
aakash123254 <aakashharwani06@gmail.com>
2025-04-21 13:01:48 +0530

    fix: added dynamic port binding for Render deploy

--- app.py
+++ app.py
@@ -203,4 +203,7 @@ def view_data():
     return jsonify(latest)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
+
